9350cc25b0fdb74a09dd3d72a3e5095d185452e1
Suzy Kirch <sqkirch@gmail.com>
2020-09-24 14:07:35 -0400

    read file completed

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,19 @@
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path, newline='') as csvfile:
+        csvread = csv.reader(csvfile, delimiter=' ')
+        matrix = []
+        for row in csvread:
+            newrow = []
+            for r in row[0].split(','):
+                if r.isnumeric():
+                    newrow += [int(r)] #want int or float??
+                else:
+                    newrow += [r]
+            matrix += [newrow]
+        return matrix
